ab52e5208b49b81b0b2203046255ff8c0380854a
Glenn Eggleton <geggleto@gmail.com>
2016-11-04 14:41:08 -0400

    Adding Validator Interface

---
+++ src/Services/ValidatorInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Glenn
+ * Date: 2016-11-04
+ * Time: 2:40 PM
+ */
+namespace G\Core\Services;
+
+
+/**
+ * Class UserValidator
+ *
+ * @package G\Services\User
+ */
+interface ValidatorInterface
+{
+    public function validate();
+
+    /**
+     * @return array|bool
+     */
+    public function getErrors();
+}
